d1ef139ae4f7b60864b2843dbbda5e8b4c44214d
thr3a <thr3a@example.com>
2023-10-31 22:21:58 +0900

    add: GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 import { MantineProvider, ColorSchemeScript, Container } from '@mantine/core';
@@ -24,6 +25,15 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
       <head>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
         <ColorSchemeScript />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5C9CZHR');
+        `}
+        </Script>
       </head>
       <body>
         <MantineProvider theme={theme}>
