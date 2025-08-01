5573cb8b90562c3f636d3cf0a4f3a118400571c6
thr3a <thr3a@example.com>
2025-08-02 01:20:31 +0900

    clean:

--- src/app/api/rp-prompt/route.ts
+++ src/app/api/rp-prompt/route.ts
@@ -1,13 +1,15 @@
 import { schema, systemPrompt } from '@/app/rp-prompt/util';
-
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { streamObject } from 'ai';
+const openai = createOpenAI({
+  baseURL: 'http://192.168.16.20:8000/v1'
+});
 
 export async function POST(req: Request) {
   const context = await req.json();
 
   const result = streamObject({
-    model: openai('gpt-4.1'),
+    model: openai.chat('main'),
     system: systemPrompt,
     prompt: context,
     schema: schema,
