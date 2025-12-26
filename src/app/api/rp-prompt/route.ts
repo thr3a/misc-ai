7a089874047ed6f1cb17323ef62ed3dc0b758bd2
thr3a <thr3a@example.com>
2025-12-27 01:49:28 +0900

    update:

--- src/app/api/rp-prompt/route.ts
+++ src/app/api/rp-prompt/route.ts
@@ -3,7 +3,7 @@ import { systemPrompt } from '@/app/rp-prompt/util';
 import { createOpenAI } from '@ai-sdk/openai';
 import { streamObject } from 'ai';
 const openai = createOpenAI({
-  baseURL: 'http://192.168.16.21:8001/v1'
+  baseURL: 'http://192.168.16.21:8000/v1'
 });
 
 export async function POST(req: Request) {
