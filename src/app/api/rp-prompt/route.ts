be38996890e66d461f289bccf3ad39198f946d42
thr3a <thr3a@example.com>
2026-01-04 21:42:32 +0900

    update:

--- src/app/api/rp-prompt/route.ts
+++ src/app/api/rp-prompt/route.ts
@@ -1,25 +1,20 @@
 import { scenarioPromptSchema } from '@/app/rp-prompt/type';
 import { systemPrompt } from '@/app/rp-prompt/util';
 import { createOpenAI } from '@ai-sdk/openai';
-import { streamObject } from 'ai';
+import { Output, streamText } from 'ai';
 const openai = createOpenAI({
   baseURL: 'http://192.168.16.21:8000/v1'
 });
 
 export async function POST(req: Request) {
   const context = await req.json();
-
-  const result = streamObject({
+  const result = streamText({
     model: openai.chat('main'),
     system: systemPrompt,
     prompt: context,
-    schema: scenarioPromptSchema,
+    output: Output.object({ schema: scenarioPromptSchema }),
     temperature: 0.3
   });
 
-  return result.toTextStreamResponse({
-    headers: {
-      'Content-Type': 'text/event-stream'
-    }
-  });
+  return result.toTextStreamResponse();
 }
